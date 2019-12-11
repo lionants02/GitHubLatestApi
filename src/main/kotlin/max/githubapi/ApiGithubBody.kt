12a5d65fb58c41a2bffffec231486808225d152b
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-12-11 15:24:18 +0700

    update GithubRelease entity

--- src/main/kotlin/max/githubapi/ApiGithubBody.kt
+++ src/main/kotlin/max/githubapi/ApiGithubBody.kt
@@ -12,16 +12,45 @@ internal val githubGson: Gson by lazy {
 }
 
 data class GithubRelease(
-    val name: String,
-    val id: Long,
+    val url: String,
     val html_url: String,
+    val assets_url: String,
+    val upload_url: String,
+    val tarball_url: String,
+    val zipball_url: String,
+    val id: Long,
+    val node_id: Long,
     val tag_name: String,
+    val target_commitish: String,
+    val name: String,
+    val body: String,
     val draft: Boolean,
     val prerelease: Boolean,
     val created_at: DateTime,
     val published_at: DateTime,
-    val assets: List<Assets>,
-    val body: String
+    val author: Author,
+    val assets: List<Assets>
+)
+
+data class Author(
+    val login: String,
+    val id: Long,
+    val node_id: String,
+    val avatar_url: String,
+    val gravatar_id: String,
+    val url: String,
+    val html_url: String,
+    val followers_url: String,
+    val following_url: String,
+    val gists_url: String,
+    val starred_url: String,
+    val subscriptions_url: String,
+    val organizations_url: String,
+    val repos_url: String,
+    val events_url: String,
+    val received_events_url: String,
+    val type: String,
+    val site_admin: Boolean
 )
 
 data class Assets(
@@ -31,5 +60,11 @@ data class Assets(
     val size: Long,
     val created_at: DateTime,
     val updated_at: DateTime,
-    val browser_download_url: String
+    val browser_download_url: String,
+    val id: Long,
+    val node_id: String,
+    val label: String,
+    val state: String,
+    val download_count: Long,
+    val uploader: Author
 )
